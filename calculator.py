86a37252f8c1fc834c4aa44478c14b974a60e713
KrishnkantSingh <87011128+KrishnkantSingh@users.noreply.github.com>
2022-04-01 09:51:22 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+a = int(input("enter first  digit"))
+b = int(input("enter the second digit"))
+c = input(" enter the operator")
+
+if c=='+':
+    print(a+b)
+elif(c=='-'):
+     print(a-b)
+elif(c=='*'):
+    print(a*b)
+elif(c=='/'):
+    print(a/b)
+
+
